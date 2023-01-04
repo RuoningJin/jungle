631574491a5ce8a376db59694303f8baf0a8ef14
RuoningJin <ronjin2013@hotmail.com>
2023-01-04 15:40:26 -0500

    test add-to-cart function

---
+++ cypress/integration/add-to-cart.spec.js
@@ -0,0 +1,32 @@
+describe('home page', () => {
+  beforeEach(() => {
+    cy.visit('users/new')
+    .get('[name="user[name]"]')
+    .type('ron')
+    .get('[name="user[email]"]')
+    .type('ron@email.com')
+    .get('[name="user[password]"]')
+    .type('123')
+    .get('[name="user[password_confirmation]"]')
+    .type('123')
+    .get('[name="commit"]')
+    .click()
+    .visit('/')
+    .get('[name="email"]')
+    .type('ron@email.com')
+    .get('[name="password"]')
+    .type('123')
+    .get('[name="commit"]')
+    .click()
+  })
+
+  it("Click on the product partial", () => {
+    cy.get('.btn')
+    .contains('Add')
+    .first()
+    .click()
+    .get('.nav-link')
+    .contains('My Cart (1)')
+    .should('be.visible')
+  });
+})
